900d0faeed9c1b0af8026652d7e40f693ac44543
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2023-05-05 00:00:33 +0600

    headline of hero

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -13,8 +13,8 @@ const Home = () => {
                 <div className="hero-overlay bg-opacity-60"></div>
                 <div className="hero-content text-center text-neutral-content">
                     <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                    <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <h1 className="mb-5 text-5xl font-bold">World Best Chefs Are There</h1>
+                    <p className="mb-5">We are there to serve you Best Experiance In Your Recipe</p>
                     <button className="btn btn-primary">Get Started</button>
                     </div>
                 </div>
