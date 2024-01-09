ecc78a5857b4649069e11e0ed0b43f67f27d9f01
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2024-01-09 07:14:04 +0600

    card editing

--- src/Components/ChefCard/ChefCard.jsx
+++ src/Components/ChefCard/ChefCard.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './ChefCard.css';
 import { Link } from 'react-router-dom';
+import { FaHeart } from "react-icons/fa";
+// import {} from "react-icons/fe"
 const ChefCard = ({chef}) => {
     return (
         <div>
@@ -10,7 +12,10 @@ const ChefCard = ({chef}) => {
                         <img src={chef.img}  alt="Shoes" className="w-full h-full" />
                     </figure>
                 </div>
-                <p id='likes'><span  className='text-primary font-extrabold text-bold align-middle text-transparent  bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>{chef.likes} </span>peoples likes him/her!</p>
+                <p id='likes' className='flex flex-row-reverse'>
+                    <span  className='text-primary font-extrabold text-bold align-middle text-transparent  bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'> {chef.likes}</span>
+                    <FaHeart className='w-6 h-6 text-red-600 mr-2'></FaHeart>
+                </p>
                 <div className="flex flex-col items-center align-middle">
                     <h2 className="card-title md:text-3xl text-2xl font-bold text-transparent  bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">{chef.name}!</h2>
                     <p><span className='font-bold'>Number Of Recipes :</span> {chef.numberOfRecipes}</p>
@@ -29,3 +34,6 @@ const ChefCard = ({chef}) => {
 };
 
 export default ChefCard;
+
+
+
