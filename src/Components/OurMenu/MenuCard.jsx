15e59eb2ed848309cb2d63a0539e5aebed7f538b
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2024-01-14 01:00:55 +0600

    Menu Card Design - Btn Css & grean btn added

--- src/Components/OurMenu/MenuCard.jsx
+++ src/Components/OurMenu/MenuCard.jsx
@@ -4,44 +4,55 @@ const MenuCard = () => {
     return (
         <div className='grid grid-cols-4 gap-[30px]'>
             <div className="card w-[277px] bg-base-100 shadow-xl">
-                <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+                <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className='w-full h-[201px] rounded-tl-[20px]'/></figure>
+                <div className="card-body text-left">
+                    <h2 className="cardTitle !text-left">fried chicken</h2>
+                    <p className='py-[21px]'>Lörem ipsum astrorör tett och kvasisade fydongen. Lörem ipsum astrorör tett och kvasisade fydongen.</p>
+                    <div className="card-actions justify-start">
+                    <button className="btn smallYellowBtn">Order Now</button>
                     </div>
                 </div>
+
             </div>
+
+
             <div className="card w-[277px] bg-base-100 shadow-xl">
-                <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+                <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className='w-full h-[201px] rounded-tl-[20px]'/></figure>
+                <div className="card-body text-left">
+                    <h2 className="cardTitle !text-left">fried chicken</h2>
+                    <p className='py-[21px]'>Lörem ipsum astrorör tett och kvasisade fydongen. Lörem ipsum astrorör tett och kvasisade fydongen.</p>
+                    <div className="card-actions justify-start">
+                    <button className="btn smallYellowBtn">Order Now</button>
                     </div>
                 </div>
+
             </div>
+            
+
             <div className="card w-[277px] bg-base-100 shadow-xl">
-                <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+                <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className='w-full h-[201px] rounded-tl-[20px]'/></figure>
+                <div className="card-body text-left">
+                    <h2 className="cardTitle !text-left">fried chicken</h2>
+                    <p className='py-[21px]'>Lörem ipsum astrorör tett och kvasisade fydongen. Lörem ipsum astrorör tett och kvasisade fydongen.</p>
+                    <div className="card-actions justify-start">
+                    <button className="btn smallYellowBtn">Order Now</button>
                     </div>
                 </div>
+
             </div>
+
+
+
             <div className="card w-[277px] bg-base-100 shadow-xl">
-                <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+                <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className='w-full h-[201px] rounded-tl-[20px]'/></figure>
+                <div className="card-body text-left">
+                    <h2 className="cardTitle !text-left">fried chicken</h2>
+                    <p className='py-[21px]'>Lörem ipsum astrorör tett och kvasisade fydongen. Lörem ipsum astrorör tett och kvasisade fydongen.</p>
+                    <div className="card-actions justify-start">
+                    <button className="btn smallYellowBtn">Order Now</button>
                     </div>
                 </div>
+
             </div>
         </div>
     );
--- src/Components/OurMenu/OurMenu.jsx
+++ src/Components/OurMenu/OurMenu.jsx
@@ -4,13 +4,20 @@ import MenuCard from './MenuCard';
 const OurMenu = () => {
     return (
         <div className='bg-[#E3F988] h-[990px]'>
-            <div className='mycontainer'>
-                <h2 className='title'>Our Menu</h2>
-                <p className='paragraph'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptatibus quasi est quisquam maxime cumque nostrum provident qui perferendis voluptas iste id, molestiae odio et dicta fugit saepe.</p>
-                <hr className='bg-[#E6E600] w-[221px] h-[2px]'/>
+            <div className='mycontainer pt-[70px]'>
+                <div className='w-[995px] px-[100px] mx-auto !text-center'>
+                    <h2 className='title'>Our Menu</h2>
+                    <p className='paragraph'>Lörem ipsum obäs tönutiktiga synade, ödleplåster, var. Tejåvis krofåsm fev, sped trafficking. Fronta pren beling, myktigt.</p>
+                    <hr className='bg-[#E6E600] w-[221px] h-[2px] mt-[30px] mb-[74.99px] mx-auto'/>
+                </div>
 
                 <div className=''>
                     <MenuCard></MenuCard>
+                    
+                </div>
+                <div className='flex justify-center'>
+                    <button className='btn greenBtn !text-center'>explore menu</button>
+
                 </div>
             </div>
             
