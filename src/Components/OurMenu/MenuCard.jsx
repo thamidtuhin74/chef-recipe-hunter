42809f2451014d1a121950ae94161af8fb0f85a3
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2024-01-14 01:31:25 +0600

    Menu Card Design - star added and design fixed

--- src/Components/OurMenu/MenuCard.jsx
+++ src/Components/OurMenu/MenuCard.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { FaStar } from 'react-icons/fa';
 
 const MenuCard = () => {
     return (
         <div className='grid grid-cols-4 gap-[30px]'>
             <div className="card w-[277px] bg-base-100 shadow-xl">
                 <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className='w-full h-[201px] rounded-tl-[20px]'/></figure>
-                <div className="card-body text-left">
+                <div className="p-5 text-left">
+                    <div className="star flex flex-row font-[18px] text-[#007500] mb-[22px]">
+                        <FaStar className='w-[18px] h-[18px]'/>
+                        <FaStar className='w-[18px] h-[18px] mx-[8px]'/>
+                        <FaStar className='w-[18px] h-[18px]'/>
+                        <FaStar className='w-[18px] h-[18px] mx-[8px]'/>
+                        <FaStar className='w-[18px] h-[18px] '/>
+                        
+                    </div>
                     <h2 className="cardTitle !text-left">fried chicken</h2>
                     <p className='py-[21px]'>Lörem ipsum astrorör tett och kvasisade fydongen. Lörem ipsum astrorör tett och kvasisade fydongen.</p>
                     <div className="card-actions justify-start">
@@ -14,11 +23,17 @@ const MenuCard = () => {
                 </div>
 
             </div>
-
-
             <div className="card w-[277px] bg-base-100 shadow-xl">
                 <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className='w-full h-[201px] rounded-tl-[20px]'/></figure>
-                <div className="card-body text-left">
+                <div className="p-5 text-left">
+                    <div className="star flex flex-row font-[18px] text-[#007500] mb-[22px]">
+                        <FaStar className='w-[18px] h-[18px]'/>
+                        <FaStar className='w-[18px] h-[18px] mx-[8px]'/>
+                        <FaStar className='w-[18px] h-[18px]'/>
+                        <FaStar className='w-[18px] h-[18px] mx-[8px]'/>
+                        <FaStar className='w-[18px] h-[18px] '/>
+                        
+                    </div>
                     <h2 className="cardTitle !text-left">fried chicken</h2>
                     <p className='py-[21px]'>Lörem ipsum astrorör tett och kvasisade fydongen. Lörem ipsum astrorör tett och kvasisade fydongen.</p>
                     <div className="card-actions justify-start">
@@ -27,11 +42,17 @@ const MenuCard = () => {
                 </div>
 
             </div>
-            
-
             <div className="card w-[277px] bg-base-100 shadow-xl">
                 <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className='w-full h-[201px] rounded-tl-[20px]'/></figure>
-                <div className="card-body text-left">
+                <div className="p-5 text-left">
+                    <div className="star flex flex-row font-[18px] text-[#007500] mb-[22px]">
+                        <FaStar className='w-[18px] h-[18px]'/>
+                        <FaStar className='w-[18px] h-[18px] mx-[8px]'/>
+                        <FaStar className='w-[18px] h-[18px]'/>
+                        <FaStar className='w-[18px] h-[18px] mx-[8px]'/>
+                        <FaStar className='w-[18px] h-[18px] '/>
+                        
+                    </div>
                     <h2 className="cardTitle !text-left">fried chicken</h2>
                     <p className='py-[21px]'>Lörem ipsum astrorör tett och kvasisade fydongen. Lörem ipsum astrorör tett och kvasisade fydongen.</p>
                     <div className="card-actions justify-start">
@@ -40,12 +61,17 @@ const MenuCard = () => {
                 </div>
 
             </div>
-
-
-
             <div className="card w-[277px] bg-base-100 shadow-xl">
                 <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className='w-full h-[201px] rounded-tl-[20px]'/></figure>
-                <div className="card-body text-left">
+                <div className="p-5 text-left">
+                    <div className="star flex flex-row font-[18px] text-[#007500] mb-[22px]">
+                        <FaStar className='w-[18px] h-[18px]'/>
+                        <FaStar className='w-[18px] h-[18px] mx-[8px]'/>
+                        <FaStar className='w-[18px] h-[18px]'/>
+                        <FaStar className='w-[18px] h-[18px] mx-[8px]'/>
+                        <FaStar className='w-[18px] h-[18px] '/>
+                        
+                    </div>
                     <h2 className="cardTitle !text-left">fried chicken</h2>
                     <p className='py-[21px]'>Lörem ipsum astrorör tett och kvasisade fydongen. Lörem ipsum astrorör tett och kvasisade fydongen.</p>
                     <div className="card-actions justify-start">
