4a7db04c083986596c5f48daaa134eae10f17092
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2024-01-13 23:12:55 +0600

    Menu Card Added

---
+++ src/Components/OurMenu/MenuCard.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+const MenuCard = () => {
+    return (
+        <div className='grid grid-cols-4 gap-[30px]'>
+            <div className="card w-[277px] bg-base-100 shadow-xl">
+                <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">Shoes!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+            <div className="card w-[277px] bg-base-100 shadow-xl">
+                <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">Shoes!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+            <div className="card w-[277px] bg-base-100 shadow-xl">
+                <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">Shoes!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+            <div className="card w-[277px] bg-base-100 shadow-xl">
+                <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">Shoes!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MenuCard;
--- src/Components/OurMenu/OurMenu.jsx
+++ src/Components/OurMenu/OurMenu.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import MenuCard from './MenuCard';
 
 const OurMenu = () => {
     return (
-        <div className='bg-[#E3F988]'>
-            <h2 className='title'>Our Menu</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptatibus quasi est quisquam maxime cumque nostrum provident qui perferendis voluptas iste id, molestiae odio et dicta fugit saepe.</p>
-            <hr className='bg-[#E6E600] w-[221px] h-[2px]'/>
+        <div className='bg-[#E3F988] h-[990px]'>
+            <div className='mycontainer'>
+                <h2 className='title'>Our Menu</h2>
+                <p className='paragraph'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptatibus quasi est quisquam maxime cumque nostrum provident qui perferendis voluptas iste id, molestiae odio et dicta fugit saepe.</p>
+                <hr className='bg-[#E6E600] w-[221px] h-[2px]'/>
+
+                <div className=''>
+                    <MenuCard></MenuCard>
+                </div>
+            </div>
             
         </div>
     );
