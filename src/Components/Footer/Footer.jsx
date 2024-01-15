eb43d1f1c8ce4969eeb6b6dbcaa4533f57e032c8
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2024-01-16 05:28:21 +0600

    Customizing footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,25 +4,43 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
     return (
         <div className="bg-[#000]">
-            <footer className="contaier mx-auto footer footer-center p-10 text-primary-content">
-            <div className="grid grid-flow-col gap-4">
-                <Link to={'/'} className="link link-hover">Home</Link> 
-                <Link to={"/login"} className="link link-hover">Login</Link> 
-                <Link to={'/register'} className="link link-hover">Register</Link> 
-                <Link className="link link-hover" to={'/blog'}>Blog</Link>
-            </div> 
-                <div>
-                    
-                    <p>Copyright © 2023 - All right reserved</p>
-                </div> 
-                <div>
-                    <div className="grid grid-flow-col gap-4">
-                    <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a> 
-                    <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a> 
-                    <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
-                    </div>
+            <footer className="mycontainer mx-auto footer footer-center p-10 text-primary-content grid grid-cols-1 md:grid-cols-3 gap-[35px]">
+
+                <div className='!text-left'>
+                    <h2 className='title !text-white'>Nipponshoku</h2>
+                    <p className='paragraph'>Nipponshoku is a website centered around Javanese Food Recipes from different
+                    chefs. Users can explore a variety of recipes from multiple chefs on the platform.
+                    Chef details are restricted and can only be accessed by logged-in users.
+                    </p>
                 </div>
+                
+                <div className="">
+                    <h2 className='title !text-white'>Important Link👇</h2>
+                    <div className='grid grid-cols-1 gap-4 paragraph'>
+                        <Link to={'/'} className="link link-hover">Home</Link> 
+                        <Link to={"/login"} className="link link-hover">Login</Link> 
+                        <Link to={'/register'} className="link link-hover">Register</Link> 
+                        <Link className="link link-hover" to={'/blog'}>Blog</Link>
+                    </div>
+                </div> 
+
+                <div className="">
+                    <h2 className='title !text-white'>Contact</h2>
+                    <div>
+                        <p className='paragraph'>call = +880 1798905167</p>
+                        <p className='paragraph'>timeing everyday 10-am to 11-pm</p>
+                        <p className='paragraph'>address xyz , n sectore 230,  waste,mumbai  1234</p>
+                    </div>
+                </div> 
+
+                
+
+                
             </footer>
+            <div className='bg-white paragraph !text-right pr-2 '>  
+                <p>Copyright © 2023 - All right reserved</p>
+                
+            </div> 
         </div>
     );
 };
