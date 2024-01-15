cd9891c7fdd9fbac14861d2514531c7fd4230c1c
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2024-01-16 05:37:17 +0600

    Customer added

--- src/Components/ReveiwSection/ReviewCard.jsx
+++ src/Components/ReveiwSection/ReviewCard.jsx
@@ -6,7 +6,9 @@ const ReviewCard = () => {
         <div className='grid  grid-cols-1 md:grid-cols-3 gap-[30px]'>
             
             <div className='bg-[#fff] rounded-[20px] p-[30px] flex flex-col items-center'>
-                <img src="/src/assets/images.png" alt="" className='w-[120px] h-[120px] rounded-[50%]'/>
+            
+
+                <img src="https://i.ibb.co/THNDjd6/buyer1.png" alt="" className='w-[120px] h-[120px] rounded-[50%]'/>
                 <div className="star flex flex-row font-[18px] text-[#007500] my-[24px]">
                     <FaStar className='w-[18px] h-[18px]'/>
                     <FaStar className='w-[18px] h-[18px] mx-[8px]'/>
@@ -19,7 +21,7 @@ const ReviewCard = () => {
             </div>
 
             <div className='bg-[#fff] rounded-[20px] p-[30px] flex flex-col items-center'>
-                <img src="/src/assets/images.png" alt="" className='w-[120px] h-[120px] rounded-[50%]'/>
+                <img src="https://i.ibb.co/9g8HVWY/buyer2.png" alt="" className='w-[120px] h-[120px] rounded-[50%]'/>
                 <div className="star flex flex-row font-[18px] text-[#007500] my-[24px]">
                     <FaStar className='w-[18px] h-[18px]'/>
                     <FaStar className='w-[18px] h-[18px] mx-[8px]'/>
@@ -28,11 +30,11 @@ const ReviewCard = () => {
                     <FaStar className='w-[18px] h-[18px] '/>
                 </div>
                 <p className='paragraph pb-[24px]'>it is a longestablished fact that a reader will be distracted by the readable content od the a page</p>
-                <h2 className='title !text-black'>richel win</h2>
+                <h2 className='title !text-black'>dina deep</h2>
             </div>
 
             <div className='bg-[#fff] rounded-[20px] p-[30px] flex flex-col items-center'>
-                <img src="/src/assets/images.png" alt="" className='w-[120px] h-[120px] rounded-[50%]'/>
+                <img src="https://i.ibb.co/r4fmVJD/buyer3.png" alt="" className='w-[120px] h-[120px] rounded-[50%]'/>
                 <div className="star flex flex-row font-[18px] text-[#007500] my-[24px]">
                     <FaStar className='w-[18px] h-[18px]'/>
                     <FaStar className='w-[18px] h-[18px] mx-[8px]'/>
@@ -41,7 +43,7 @@ const ReviewCard = () => {
                     <FaStar className='w-[18px] h-[18px] '/>
                 </div>
                 <p className='paragraph text-center pb-[24px]'>it is a longestablished fact that a reader will be distracted by the readable content od the a page</p>
-                <h2 className='title !text-black'>richel win</h2>
+                <h2 className='title !text-black'>devid jay</h2>
             </div>
         </div>
     );
