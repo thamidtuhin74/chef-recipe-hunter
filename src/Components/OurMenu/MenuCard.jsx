b43f93105fcc093a8a045630fac2acd0917b32d5
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2024-01-16 05:52:19 +0600

    image added in menu section

--- src/Components/OurMenu/MenuCard.jsx
+++ src/Components/OurMenu/MenuCard.jsx
@@ -5,7 +5,7 @@ const MenuCard = () => {
     return (
         <div className='grid justify-center lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-[30px] mx-auto'>
             <div className="card w-[277px] max-h-[483px] bg-base-100 shadow-xl">
-                <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className='w-full h-[201px] rounded-tl-[20px]'/></figure>
+                <figure><img src="https://i.ibb.co/fvK1nf6/chicken-salad.png" alt="Shoes" className='w-full h-[201px] rounded-tl-[20px]'/></figure>
                 <div className="p-5 text-left">
                     <div className="star flex flex-row font-[18px] text-[#007500] mb-[22px]">
                         <FaStar className='w-[18px] h-[18px]'/>
@@ -24,7 +24,7 @@ const MenuCard = () => {
 
             </div>
             <div className="card w-[277px] max-h-[483px] bg-base-100 shadow-xl">
-                <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className='w-full h-[201px] rounded-tl-[20px]'/></figure>
+                <figure><img src="https://i.ibb.co/zJ4Sbd5/fried-chicken.png" alt="Shoes" className='w-full h-[201px] rounded-tl-[20px]'/></figure>
                 <div className="p-5 text-left">
                     <div className="star flex flex-row font-[18px] text-[#007500] mb-[22px]">
                         <FaStar className='w-[18px] h-[18px]'/>
@@ -43,7 +43,7 @@ const MenuCard = () => {
 
             </div>
             <div className="card w-[277px] max-h-[483px] bg-base-100 shadow-xl">
-                <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className='w-full h-[201px] rounded-tl-[20px]'/></figure>
+                <figure><img src="https://i.ibb.co/CKCFYzs/spicey-kabab.png" alt="Shoes" className='w-full h-[201px] rounded-tl-[20px]'/></figure>
                 <div className="p-5 text-left">
                     <div className="star flex flex-row font-[18px] text-[#007500] mb-[22px]">
                         <FaStar className='w-[18px] h-[18px]'/>
@@ -62,7 +62,7 @@ const MenuCard = () => {
 
             </div>
             <div className="card w-[277px] max-h-[483px] bg-base-100 shadow-xl">
-                <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className='w-full h-[201px] rounded-tl-[20px]'/></figure>
+                <figure><img src="https://i.ibb.co/5RwPYGZ/spicey-roll.png" alt="Shoes" className='w-full h-[201px] rounded-tl-[20px]'/></figure>
                 <div className="p-5 text-left">
                     <div className="star flex flex-row font-[18px] text-[#007500] mb-[22px]">
                         <FaStar className='w-[18px] h-[18px]'/>
